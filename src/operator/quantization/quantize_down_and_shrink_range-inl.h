adae4b7e7f534cff28e2501df035d87a077cf47e
reminisce <wujun.nju@gmail.com>
2017-12-22 15:59:06 -0800

    Allocate temp space only once for quantized_conv2d

--- src/operator/quantization/quantize_down_and_shrink_range-inl.h
+++ src/operator/quantization/quantize_down_and_shrink_range-inl.h
@@ -17,21 +17,9 @@ namespace mxnet {
 namespace op {
 
 struct QuantizeDownAndShrinkRangeParam : public dmlc::Parameter<QuantizeDownAndShrinkRangeParam> {
-  dmlc::optional<int> min_qval;  // min quantized value calculated from calibration set
-  dmlc::optional<int> max_qval;  // max quantized value calculated from calibration set
   dmlc::optional<float> min_fval;  // min float value calculated from calibration set
   dmlc::optional<float> max_fval;  // max float value calculated from calibration set
   DMLC_DECLARE_PARAMETER(QuantizeDownAndShrinkRangeParam) {
-    DMLC_DECLARE_FIELD(min_qval)
-    .set_default(dmlc::optional<int>())
-    .describe("Min threshold calculated from calibration dataset and used for quantizing"
-              " down output values from quantized operators. If not set, min output"
-              " values will be calculated on the fly.");
-    DMLC_DECLARE_FIELD(max_qval)
-    .set_default(dmlc::optional<int>())
-    .describe("Max threshold calculated from calibration dataset and used for quantizing"
-              " down output values from quantized operators. If not set, max output"
-              " values will be calculated on the fly.");
     DMLC_DECLARE_FIELD(min_fval)
     .set_default(dmlc::optional<float>());
     DMLC_DECLARE_FIELD(max_fval)
