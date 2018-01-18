95c27ddb0b7a788404b97cc3ab0ed3ebd72e8bdc
reminisce <wujun.nju@gmail.com>
2018-01-17 23:20:20 -0800

    Merge quantize and dequantize with master impl

--- src/operator/quantization/quantize-inl.h
+++ src/operator/quantization/quantize-inl.h
@@ -41,22 +41,24 @@ struct QuantizeParam : public dmlc::Parameter<QuantizeParam> {
   DMLC_DECLARE_PARAMETER(QuantizeParam) {
     DMLC_DECLARE_FIELD(out_type)
     .add_enum("int8", mshadow::kInt8)
-    .set_default(mshadow::kInt8)
+    .add_enum("uint8", mshadow::kUint8)
+    .set_default(mshadow::kUint8)
     .describe("Output data type.");
   }
 };
 
-struct quantize {
+// quantize float to uint8_t
+struct quantize_unsigned {
   template<typename DstDType, typename SrcDType>
   MSHADOW_XINLINE static void Map(int i, DstDType *out, float *omin_range,
                                   float *omax_range, const SrcDType *in,
-                                  const float *imin_range, const float *imax_range) {
+                                  const float *imin_range, const float *imax_range,
+                                  const double min_limit, const double max_limit) {
     using mshadow::red::limits::MinValue;
     using mshadow::red::limits::MaxValue;
-    float scale = (MaxValue<DstDType>() - MinValue<DstDType>()) /
-                  (*imax_range - *imin_range);
-    out[i] = static_cast<DstDType>((in[i] - *imin_range) * scale + 0.5) +
-        MinValue<DstDType>();
+    //const float scale = (MaxValue<DstDType>() - MinValue<DstDType>()) / (*imax_range - *imin_range);
+    const float scale = (max_limit - min_limit) / (*imax_range - *imin_range);
+    out[i] = static_cast<DstDType>((in[i] - *imin_range) * scale + 0.5);
     *omin_range = *imin_range;
     *omax_range = *imax_range;
   }
@@ -64,15 +66,13 @@ struct quantize {
 
 
 // keep zero-center
-struct quantize_v2 {
+struct quantize_zero_centered {
   template<typename DstDType, typename SrcDType>
   MSHADOW_XINLINE static void Map(int i, DstDType *out, float *omin_range,
                                   float *omax_range, const SrcDType *in,
-                                  const float *imin_range, const float *imax_range) {
-    using mshadow::red::limits::MinValue;
-    using mshadow::red::limits::MaxValue;
+                                  const float *imin_range, const float *imax_range,
+                                  const float quantized_range) {
     float real_range = MaxAbs(*imin_range, *imax_range);
-    float quantized_range = MinAbs(MaxValue<DstDType>(), MinValue<DstDType>());
     float scale = quantized_range / real_range;
     SrcDType x = in[i];
     out[i] = static_cast<DstDType>(
@@ -90,14 +90,24 @@ void QuantizeCompute(const nnvm::NodeAttrs& attrs,
                      const std::vector<TBlob>& outputs) {
   using namespace mshadow;
   using namespace mxnet_op;
+  using mshadow::red::limits::MinValue;
+  using mshadow::red::limits::MaxValue;
   Stream<xpu> *s = ctx.get_stream<xpu>();
 
   const QuantizeParam& param = nnvm::get<QuantizeParam>(attrs.parsed);
-  typedef float  SrcDType;
-  typedef int8_t DstDType;
-  Kernel<quantize_v2, xpu>::Launch(s, outputs[0].Size(),
-    outputs[0].dptr<DstDType>(), outputs[1].dptr<float>(), outputs[2].dptr<float>(),
-    inputs[0].dptr<SrcDType>(), inputs[1].dptr<float>(), inputs[2].dptr<float>());
+  if (param.out_type == mshadow::kUint8) {
+    Kernel<quantize_unsigned, xpu>::Launch(s, outputs[0].Size(),
+      outputs[0].dptr<uint8_t>(), outputs[1].dptr<float>(), outputs[2].dptr<float>(),
+      inputs[0].dptr<float>(), inputs[1].dptr<float>(), inputs[2].dptr<float>(),
+      MinValue<uint8_t>(), MaxValue<uint8_t>());
+  } else if (param.out_type == mshadow::kInt8) {  // zero-centered quantization
+    Kernel<quantize_zero_centered, xpu>::Launch(s, outputs[0].Size(),
+      outputs[0].dptr<int8_t>(), outputs[1].dptr<float>(), outputs[2].dptr<float>(),
+      inputs[0].dptr<float>(), inputs[1].dptr<float>(), inputs[2].dptr<float>(),
+      MinAbs(MaxValue<int8_t>(), MinValue<int8_t>()));
+  } else {
+    LOG(FATAL) << "quantize op only supports int8 and uint8 as output type";
+  }
 }
 
 inline bool QuantizeShape(const nnvm::NodeAttrs& attrs,
@@ -121,11 +131,17 @@ inline bool QuantizeType(const nnvm::NodeAttrs& attrs,
                          std::vector<int> *out_attrs) {
   CHECK_EQ(in_attrs->size(), 3U);
   CHECK_EQ(out_attrs->size(), 3U);
-
+  const QuantizeParam& param = nnvm::get<QuantizeParam>(attrs.parsed);
   TYPE_ASSIGN_CHECK(*in_attrs, 0, mshadow::kFloat32);
   TYPE_ASSIGN_CHECK(*in_attrs, 1, mshadow::kFloat32);
   TYPE_ASSIGN_CHECK(*in_attrs, 2, mshadow::kFloat32);
-  TYPE_ASSIGN_CHECK(*out_attrs, 0, mshadow::kInt8);
+  if (param.out_type == mshadow::kUint8) {
+    TYPE_ASSIGN_CHECK(*out_attrs, 0, mshadow::kUint8);
+  } else if (param.out_type == mshadow::kInt8) {
+    TYPE_ASSIGN_CHECK(*out_attrs, 0, mshadow::kInt8);
+  } else {
+    LOG(FATAL) << "quantize op only supports int8 and uint8 as output type";
+  }
   TYPE_ASSIGN_CHECK(*out_attrs, 1, mshadow::kFloat32);
   TYPE_ASSIGN_CHECK(*out_attrs, 2, mshadow::kFloat32);
   return (*in_attrs)[0] != -1;
