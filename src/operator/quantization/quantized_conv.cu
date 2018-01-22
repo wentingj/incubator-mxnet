c3473602a48721733e4dcc076ad5b8219eec0b7c
reminisce <wujun.nju@gmail.com>
2018-01-22 11:09:12 -0800

    Add license header

--- src/operator/quantization/quantized_conv.cu
+++ src/operator/quantization/quantized_conv.cu
@@ -1,3 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 /*!
  * Copyright (c) 2017 by Contributors
  * \file quantized_conv.cu
@@ -108,7 +127,7 @@ class QuantizedCuDNNConvOp {
                     TShape({fshape[N], fshape[H], fshape[W], fshape[C]}),
                     dev_mask, DataType<SrcType>::kFlag, dev_id);
       temp_dptr += weight_size * sizeof(SrcType);
-      
+
       // input:  [NCHW] => [NHWC](batch, in_height, in_width, in_channels)
       // filter: [NCHW] => [NHWC](out_channels, filter_height, filter_width, in_channels)
       TransposeImpl<gpu>(ctx.run_ctx, data,   data_,   TShape({N, H, W, C}));
