2ee7949e005833f82d6536754498585b5e2ed77f
wentingj <wenting.jiang@intel.com>
2018-03-05 16:03:37 +0800

    add quantize unsigned int8 mkldnn implementation, then accuracy will be normal. fp32 vs int8 topk:0.91249999999999998/0.91249999999999998

--- src/operator/quantization/mkldnn/mkldnn_quantize-inl.h
+++ src/operator/quantization/mkldnn/mkldnn_quantize-inl.h
@@ -29,7 +29,74 @@ namespace mxnet {
 namespace op {
 
 template<typename SrcType, typename DstType>
-void MKLQuantizeComputeKer(const nnvm::NodeAttrs& attrs,
+void MKLQuantizeComputeUnsigned(const nnvm::NodeAttrs& attrs,
+                     const OpContext& ctx,
+                     const std::vector<TBlob>& inputs,
+                     const std::vector<OpReqType>& req,
+                     const std::vector<TBlob>& outputs) {
+  using namespace mshadow;
+  using namespace mxnet_op;
+  using red::limits::MaxValue;
+  using red::limits::MinValue;
+  Stream<cpu> *s = ctx.get_stream<cpu>();
+  const QuantizeParam& param = nnvm::get<QuantizeParam>(attrs.parsed);
+
+  // check shapes
+  int i_dim = inputs[0].ndim();
+  int o_dim = outputs[0].ndim();
+
+  CHECK_EQ(i_dim, o_dim);
+
+  int total_len = 1;
+  memory::dims tensor_shape;
+  for (size_t ii = 0; ii < i_dim; ++ii) {
+    CHECK_EQ(inputs[0].size(ii), outputs[0].size(ii));
+    total_len *= inputs[0].size(ii);
+  }
+  tensor_shape.push_back(total_len);
+
+  //const float scale = (max_limit - min_limit) / (*imax_range - *imin_range);
+  //out[i] = static_cast<DstDType>((in[i] - *imin_range) * scale + 0.5);
+  //*omin_range = *imin_range;
+  //*omax_range = *imax_range;
+  float real_range = MaxAbs(*inputs[1].dptr<float>(), *inputs[2].dptr<float>());
+  float quantized_range = MaxAbs(MaxValue<DstType>(), MinValue<DstType>());
+  float scale = quantized_range / real_range;
+  //*outputs[1].dptr<float>() = MinAbs(*inputs[1].dptr<float>(), *inputs[2].dptr<float>());
+  *outputs[1].dptr<float>() = 0.0;//MinAbs(*inputs[1].dptr<float>(), *inputs[2].dptr<float>());
+  *outputs[2].dptr<float>() = real_range;
+  //std::cout<<"--MKLQuantizeComputeKerUint8: real_range="<<real_range<<std::endl;
+  //std::cout<<"                       : inputs[1]="<<inputs[1].dptr<float>()[0]<<std::endl;
+  //std::cout<<"                       : inputs[2]="<<inputs[2].dptr<float>()[0]<<std::endl;
+  //std::cout<<"                       : quantized_range="<<quantized_range<<std::endl;
+
+  primitive_attr attr;
+  int mask = 0;
+  std::vector<float> scales = {scale};
+  attr.set_output_scales(mask, scales);
+  attr.set_int_output_round_mode(round_nearest);
+ 
+  mkldnn::engine cpu_engine = mxnet::CpuEngine::Get()->get_engine();
+  
+  auto i_mpd = memory::primitive_desc({tensor_shape,
+                                      (mkldnn::memory::data_type)data_type_enum<SrcType>::type,
+                                       memory::format::x},
+                                       cpu_engine);
+  auto o_mpd = memory::primitive_desc({tensor_shape,
+                                      (mkldnn::memory::data_type)data_type_enum<DstType>::type,
+                                       memory::format::x},
+                                       cpu_engine);
+  
+   auto reorder_pd  = reorder::primitive_desc(i_mpd, o_mpd, attr);
+   auto input = memory(i_mpd, inputs[0].dptr<SrcType>());
+   auto output = memory(o_mpd, outputs[0].dptr<DstType>());
+  
+   auto r = reorder(reorder_pd, input, output);
+   stream(stream::kind::lazy).submit({r}).wait();
+}
+
+template<typename SrcType, typename DstType>
+void MKLQuantizeComputeZeroCenter(const nnvm::NodeAttrs& attrs,
                      const OpContext& ctx,
                      const std::vector<TBlob>& inputs,
                      const std::vector<OpReqType>& req,
@@ -61,6 +128,8 @@ void MKLQuantizeComputeKer(const nnvm::NodeAttrs& attrs,
   *outputs[1].dptr<float>() = -real_range;
   *outputs[2].dptr<float>() = real_range;
   //std::cout<<"--MKLQuantizeComputeKer: real_range="<<real_range<<std::endl;
+  //std::cout<<"                       : inputs[1]="<<inputs[1].dptr<float>()[0]<<std::endl;
+  //std::cout<<"                       : inputs[2]="<<inputs[2].dptr<float>()[0]<<std::endl;
   //std::cout<<"                       : quantized_range="<<quantized_range<<std::endl;
 
   primitive_attr attr;
@@ -98,20 +167,20 @@ void MKLQuantizeCompute(const nnvm::NodeAttrs& attrs,
   auto out_type = param.out_type;
   //CHECK_EQ(out_type, mshadow::kUint8);
   if (param.out_type == mshadow::kUint8) {
-    MKLQuantizeComputeKer<float, uint8_t>(attrs,
-                                       ctx,
-                                       inputs,
-                                       req,
-                                       outputs
-                                       );
+    MKLQuantizeComputeUnsigned<float, uint8_t>(attrs,
+                                              ctx,
+                                              inputs,
+                                              req,
+                                              outputs
+                                              );
   }                                     
   else { 
-    MKLQuantizeComputeKer<float, int8_t>(attrs,
-                                       ctx,
-                                       inputs,
-                                       req,
-                                       outputs
-                                       );
+    MKLQuantizeComputeZeroCenter<float, int8_t>(attrs,
+                                                 ctx,
+                                                 inputs,
+                                                 req,
+                                                 outputs
+                                                 );
   }                                       
 }
 }  // namespace op
